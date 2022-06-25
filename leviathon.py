874c3311962f5efae318498bc089b3b67ba921fa
xts-sec <mattcocklin@protonmail.com>
2022-06-24 22:12:17 -0400

    ascii art banner

--- leviathon.py
+++ leviathon.py
@@ -6,6 +6,18 @@ import os
 from pprint import pprint
 import pandas as pd
 
+print("""
+
+  _      ________      _______       _______ _    _  ____  _   _ 
+ | |    |  ____\ \    / /_   _|   /\|__   __| |  | |/ __ \| \ | |
+ | |    | |__   \ \  / /  | |    /  \  | |  | |__| | |  | |  \| |
+ | |    |  __|   \ \/ /   | |   / /\ \ | |  |  __  | |  | | . ` |
+ | |____| |____   \  /   _| |_ / ____ \| |  | |  | | |__| | |\  |
+ |______|______|   \/   |_____/_/    \_\_|  |_|  |_|\____/|_| \_|
+                                                                 
+Made by xts-sec (https://github.com/xts-sec)
+""")
+
 parser = argparse.ArgumentParser(description='An offensive security tool which automates an engagement as far as possible, including scanning, enumeration, and exploitation.')
 
 parser.add_argument("target", type=str)
@@ -14,12 +26,11 @@ args = parser.parse_args()
 
 nm = nmap.PortScanner()
 
-def div(len):
-    print("#" * len)
-    return(0)
+def div():
+    print("#" * 50)
 
 def quickScanTCP(target):
-    div(20)
+    div()
     nm.scan(target, arguments="")
     for host in nm.all_hosts():
         print('Host : %s (%s)' % (host, nm[host].hostname()))
@@ -30,10 +41,10 @@ def quickScanTCP(target):
         lport = nm[host][proto].keys()
         for port in lport:
             print('port : %s' % (port))
-    div(20)
+    div()
 
 def serviceScanTCP(target):
-    div(20)
+    div()
     nm.scan(target, arguments="-sV")
     for host in nm.all_hosts():
         print('Host : %s (%s)' % (host, nm[host].hostname()))
@@ -44,7 +55,7 @@ def serviceScanTCP(target):
         lport = nm[host][proto].keys()
         for port in lport:
             print('port : %s\tService : %s\tVersion : %s' % (port, nm[host][proto][port]['name'], nm[host][proto][port]['product'] + " " + nm[host][proto][port]['version']))
-    div(20)
+    div()
 
 def menu():
     while True:
@@ -53,7 +64,7 @@ def menu():
         print("Press 1 for quick tcp scan")
         print("Press 2 for Service Scan")
 
-        div(20)
+        div()
 
         choice = input (">>> ")
 
